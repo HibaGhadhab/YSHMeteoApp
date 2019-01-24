64d313ad74792617f0a84709ef6a3856afe6d54b
HibaGhadhab <hibaghadhab@ensg.eu>
2019-01-24 13:35:41 +0100

    using cors

--- app.js
+++ app.js
@@ -3,10 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-
 var cors = require('cors');
+var indexRouter = require('./routes/index');
 
 var app = express();
 
@@ -19,7 +17,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
