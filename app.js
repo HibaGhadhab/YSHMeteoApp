9714ac656b17999dba3a5de9c9210a8c09f1a103
HibaGhadhab <hibaghadhab@ensg.eu>
2019-01-29 17:35:10 +0100

    adding API documentation Swagger

--- app.js
+++ app.js
@@ -8,6 +8,16 @@ var indexRouter = require('./routes/index');
 
 var app = express();
 
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
+ 
+var options = {
+  customCss: '.swagger-ui .topbar { display: none }'
+};
+ 
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument, options));
+
+
 app.use(cors());
 
 app.use(logger('dev'));
